1b17ab64d2b1eb15e1208f0557580a1d0aba06c2
Devansh-kajve <devanshkajve123@gmail.com>
2022-07-26 20:04:43 +0530

    changed cost

--- contracts/Land.sol
+++ contracts/Land.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Land is ERC721 {
-    uint256 public cost = 0.1 ether;
+    uint256 public cost = 100 finney;
     uint256 public maxSupply = 6;
     uint256 public totalSupply = 0;
 
--- migrations/2_deploy_land.js
+++ migrations/2_deploy_land.js
@@ -4,7 +4,7 @@ module.exports = async function (deployer) {
 
     const NAME = "Samsara Building"
     const SYMBOL = "SB"
-    const COST = web3.utils.toWei('0.1','ether')
+    const COST = web3.utils.toWei('100','finney')
   await deployer.deploy(Land, NAME, SYMBOL, COST);
 };
  
